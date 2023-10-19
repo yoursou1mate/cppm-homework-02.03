3504cd939facd60ef758ce26a07c3f31ca87a47f
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-10-19 14:23:28 +0300

    Task fixed1

--- cppm-homework-02.03/main.cpp
+++ cppm-homework-02.03/main.cpp
@@ -15,25 +15,37 @@ struct address
     unsigned int House;
     unsigned int Appartment;
     unsigned int Index;
+    
+//    address(const std::string& aCity, const std::string& aStreet, unsigned int aHouse, unsigned int aAppartment, unsigned int aIndex):
+//    City { aCity }, Street { aStreet }, House { aHouse }, Appartment { aAppartment }, Index { aIndex} {}
 };
 
-void printAddress (struct address Person)
+void printAddress (struct address& Person)
 {
-    std::cout << "City: ";
-    std::cin >> Person.City;
-    std::cout << "Street: ";
-    std::cin >> Person.Street;
-    std::cout << "House: ";
-    std::cin >> Person.House;
-    std::cout << "Appartment: ";
-    std::cin >> Person.Appartment;
-    std::cout << "Index: ";
-    std::cin >> Person.Index;
+    std::cout << "City: " << Person.City << '\n';
+    std::cout << "Street: " << Person.Street <<'\n';
+    std::cout << "House: " << Person.House << '\n';
+    std::cout << "Appartment: " << Person.Appartment << '\n';
+    std::cout << "Index: " << Person.Index << '\n';
+    
 }
+
 int main(int argc, const char * argv[]) {
    
-    int i = 2;
+    int i = 0;
     address* newAddress = new address[i];
+    
+    newAddress[0].City = "Moscow";
+    newAddress[0].Street = "Rodshelskaya";
+    newAddress[0].House = 15;
+    newAddress[0].Appartment = 1;
+    newAddress[0].Index = 101010;
+    
+    newAddress[1].City = "Nizhny Novgorod";
+    newAddress[1].Street = "Minina";
+    newAddress[1].House = 1;
+    newAddress[1].Appartment = 2;
+    newAddress[1].Index = 600001;
     
     printAddress(newAddress[0]);
     std::cout << '\n';
